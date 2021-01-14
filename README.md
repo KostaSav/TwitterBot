526bcd47436522f7f6be5b7a871627a64c995e94
KostaSav <ksavvidis@outlook.com>
2021-01-14 19:14:03 +0200

    Improved functionality, added retweet feature and updated README

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # TwitterBot
 
-A twitter bot with python using the selenium library and GeckoDriver
+A twitter bot with python using the selenium library and GeckoDriver.<br/>
+Using a keyword, the bot uses it to perform a search on Twitter and interacts with the results(tweets). For the moment, it likes and retweets these tweets.<br/>
 
-Based on Dev Ed's video tutorial (https://www.youtube.com/watch?v=7ovFudqFB0Q)
-
-_the tutorial code is not entirely functional any more, because of Twitter's changes in their DOM tree_
+Based on Dev Ed's [video tutorial](https://www.youtube.com/watch?v=7ovFudqFB0Q).<br/>
+_(the tutorial code is not entirely functional any more, because of Twitter's changes in their DOM tree)_
 
 ## Installation
 
@@ -19,12 +19,14 @@ pip install selenium
 ## Bugs:
 
 - if a tweet is already liked, an exception is thrown and a message is printed, but it does not break
-- again, if a tweet is already liked, the bot might like a comment instead
+- **ATTENTION:** if Twitter detects suspicious activity after many login attempts, login fails but no exception is thrown, the bot tries (unsuccessfully) to like/retweet without an account
+- ~~again, if a tweet is already liked, the bot might like a comment instead~~
 - ~~even if the sign in is unsuccessful, the program still searches the given word and tries to like tweets~~
 
 ## TODO:
 
-1. [ ] retweet
-2. [ ] comment
-3. [ ] follow
-4. [ ] ChromeDriver
+1. [x] like
+2. [x] retweet
+3. [ ] comment
+4. [ ] follow
+5. [ ] ChromeDriver
--- app.py
+++ app.py
@@ -1,39 +1,46 @@
 # A twitter bot with python using the selenium library and GeckoDriver
-# Based on Dev Ed's video tutorial (https://www.youtube.com/watch?v=7ovFudqFB0Q)
-# Just a comment to test Git Bash commit
-
+# Inspired by Dev Ed's video tutorial (https://www.youtube.com/watch?v=7ovFudqFB0Q) - the Twitter DOM tree has changed, so there are quite some changes
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
-# Your login data and the keyword you want to search for in Twitter
-username = "your_phone_email_or_username"
-password = "your_password"
+### Your login data and the keyword you want to search for in Twitter
+### Make sure you don't share your credentials on GitHub or anywhere else!
+username = ""
+password = ""
 searchTerm = "giveaway"
 comment = "Nice"
 
-
+### A class containing the various actions the bot can execute
 class TwitterBot:
+    ## Initialize our bot's data
     def __init__(self, username, password):
+        print("Initializing bot and maximizing window now. Waiting 3 seconds...")
         self.username = username
         self.password = password
         self.bot = webdriver.Firefox()
         self.bot.maximize_window()
-
-    def login(self):
-        bot = self.bot
-        bot.get("https://twitter.com/")
         time.sleep(3)
+
+    ## Input bot's data and login to Twitter
+    def login(self):
+        # Get the website containing the login forms, wait for it to load and then get the html username and password fields
+        print("Entering credentials, waiting 5 seconds.....")
+        bot = self.bot
+        bot.get("https://twitter.com/login")
+        time.sleep(5)
         username = bot.find_element_by_name("session[username_or_email]")
         password = bot.find_element_by_name("session[password]")
         # If anything is written inside the login forms, erase it
         username.clear()
         password.clear()
+        # Fill username and password with our data and press ENTER to login
         username.send_keys(self.username)
         password.send_keys(self.password)
-        password.send_keys(Keys.RETURN)  # click the ENTER key to login
+        password.send_keys(Keys.RETURN)
         time.sleep(5)
         loginError = bot.find_elements_by_xpath(
             "//span[contains(text(), 'match our records')]"
@@ -49,8 +56,7 @@ class TwitterBot:
         print(loginError)
         time.sleep(5)
 
-    # Scroll until the element we want to interact with (Like button, Retweet button, etc.) is inside the viewport
-
+    ## Scroll until the element we want to interact with (Like button, Retweet button, etc.) is inside the viewport
     def scroll_shim(self, passed_in_driver, object):
         x = object.location["x"]
         y = object.location["y"]
@@ -59,14 +65,16 @@ class TwitterBot:
         passed_in_driver.execute_script(scroll_by_coord)
         passed_in_driver.execute_script(scroll_nav_out_of_way)
 
+    ## Give a like/heart to a tweet containing our term
     def like_tweet(self, term):
         links = list()
         counter = 1  # count the retrieved tweets
         bot = self.bot
+        print('Searching for the term "' + term + '", waiting 5 seconds.....')
         bot.get("https://twitter.com/search?q=" + term + "&src=typd")
         time.sleep(5)
 
-        # Loop how many times we scroll down and new tweets are loaded
+        # Loop how many times we scroll down and load more tweets
         for i in range(1, 3):
             # The tweet's url can be found in several positions in the DOM
             # tree, we fetch it once from the anchor tag containing the
@@ -74,54 +82,107 @@ class TwitterBot:
             tweets = bot.find_elements_by_css_selector(
                 'a[href*="status"]:not([href*=photo]):not([href*=retweets]):not([href*=likes]):not([href*=media_tags])'
             )
+            print("Getting tweet links, waiting 2 seconds..")
             time.sleep(2)
 
             # Get the tweet's actual url and store them in a list
             tempLoopLinks = [elem.get_attribute("href") for elem in tweets]
             for x in tempLoopLinks:
                 links.append(x)
+            print("Storing tweets, waiting 2 seconds..")
             time.sleep(2)
-            print("Loop " + str(i) + " complete!")
+            print("Scroll loop " + str(i) + " complete!")
 
             # Scroll down in order to load new tweets
+            print("Scrolling, waiting 10 seconds..........")
             bot.execute_script("window.scrollTo(0,document.body.scrollHeight)")
             time.sleep(10)
 
-        # Locate and click the Like button
+        ## Locate and click the Like and Retweet buttons
         links = list(dict.fromkeys(links))  # Remove duplicates
         for link in links:
+            print("Found link, waiting 10 seconds..........")
             print(counter, link)
             bot.get(link)
             time.sleep(10)
             try:
                 # Find the Like button
+                print("Looking for like button, waiting 2 seconds..")
                 toLike = bot.find_element_by_css_selector(
                     'div[aria-label="Like"][role="button"][data-testid="like"]'
                 )
-                # print(toLike)
-                time.sleep(5)
+                time.sleep(2)
 
                 # Scroll so that it is in our viewport
+                print(
+                    "Scrolling a bit to find the correct like/retweet buttons, waiting 2 seconds.."
+                )
                 if "firefox" in bot.capabilities["browserName"]:
                     self.scroll_shim(bot, toLike)
+
+                # Click the Like button
                 actions = ActionChains(bot)
                 actions.move_to_element(toLike)
                 actions.click()
                 actions.perform()
+                time.sleep(2)
+
+                # Find the Retweet button
+                print("Looking for retweet button arrows, waiting 2 seconds..")
+                toRetweet = bot.find_element_by_css_selector(
+                    'div[aria-label="Retweet"][role="button"][data-testid="retweet"]'
+                )
+                time.sleep(2)
+
+                # Click the Retweet button
+                actions = ActionChains(bot)
+                actions.move_to_element(toRetweet)
+                actions.click()
+                actions.perform()
+                time.sleep(2)
+
+                # Find the Confirm Retweet button
+                print('Looking for "Confirm Retweet", waiting 2 seconds..')
+                toConfirmRetweet = bot.find_element_by_css_selector(
+                    'div[aria-label="Retweet"][role="button"][data-testid="retweet"]'
+                )
+                time.sleep(2)
+
+                # Click the Confirm Retweet button
+                actions = ActionChains(bot)
+                actions.move_to_element(toConfirmRetweet)
+                actions.click()
+                actions.perform()
+                time.sleep(2)
 
                 # Show a confirmation in console
-                time.sleep(2)
-                print("Gave a LIKE to tweet " + str(counter))
+                print(
+                    "Gave a LIKE and a RT to tweet "
+                    + str(counter)
+                    + ". Waiting 5 seconds..."
+                )
+                print(
+                    "********************************************************************************"
+                )
                 time.sleep(5)
             except Exception as ex:
                 # At the moment, an exception will be thrown
                 # when a tweet is already liked
-                print("Exception...sleeping for a bit...")
+                print(
+                    "Exception, the tweet might already have a like/retweet. Sleeping for 5 seconds....."
+                )
                 print(ex)
-                time.sleep(10)
+                print(
+                    "********************************************************************************"
+                )
+                time.sleep(5)
 
             counter = counter + 1
             time.sleep(1)
+        print(
+            "--------------------------------------------------------------------------------"
+        )
+        print("Finished liking/retweeting. Goodbye!")
 
 
 # Our bot's name is bix
